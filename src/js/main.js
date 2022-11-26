63c80937310e6f33e7c1ed40bcba0c3eebde81cc
Vasili Homcenco <homcenco@gmail.com>
2022-11-26 20:32:11 +0300

    Fix BEM

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,16 @@
 function toggleMenu() {
-  let button = document.getElementsByClassName("header__menu-button");
-  let items = document.getElementsByClassName("header__menu-items");
-  let layout = document.getElementsByClassName("bg__menu-layout");
-  button[0].classList.toggle("active");
-  items[0].classList.toggle("active");
-  layout[0].classList.toggle("active");
+  let menuBurger = document.getElementsByClassName("header__menu-burger");
+  let menuItems = document.getElementsByClassName("header__menu-items");
+  let menuLayout = document.getElementsByClassName("layout__header-menu");
+  menuBurger[0].classList.toggle("active");
+  menuItems[0].classList.toggle("active");
+  menuLayout[0].classList.toggle("active");
 }
 
 function makeActive(a) {
   if (window.innerWidth < 768) {
     let parent = a.parentNode;
-    let menu = parent.querySelector(".dropdown__menu, .dropdown__submenu");
+    let menu = parent.querySelector(".header__menu-dropdown");
     a.classList.toggle("active");
     parent.classList.toggle("active");
     menu.classList.toggle("active");
